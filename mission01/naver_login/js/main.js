4274a0d20a9d2e710deced28a1813c87a30cdf3b
jiyoung <aa991102@naver.com>
2024-11-24 00:35:16 +0900

    네이버 로그인 - main.js 수정

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -3,70 +3,84 @@ const user = {
   pw: "spdlqj123!@",
 };
 
-// 로그인 버튼 클릭 시 발생하는 이벤트
-function handlLogin(e) {
-  const inputId = getNode("#userEmail");
-  const inputPw = getNode("#userPassword");
-  const loginState = getNode("#loginState");
-  const loginIp = getNode("#loginIp");
-  let isValid = false; // id(email), pw 유효한 값 입력 여부
+/*
+1. email 정규표현식을 사용한 조건처리 
+   - false면 해당 input에 is--invalid 추가
+   - true면 해당 input에 is--invalid 제거
 
-  // email validation
-  if (emailReg(inputId.value)) {
-    inputId.classList.remove("is--invalid");
-    isValid = true;
-  } else {
-    inputId.classList.add("is--invalid");
-  }
+2. pw 정규표현식을 사용한 validation
+   - false면 해당 input에 is--invalid 추가
+   - true면 해당 input에 is--invalid 제거
 
-  // pw validation
-  if (pwReg(inputPw.value)) {
-    inputPw.classList.remove("is--invalid");
-  } else {
-    isValid = false;
-    inputPw.classList.add("is--invalid");
-  }
+3. 로그인 버튼을 클릭시 user.id의 값과 input의 값을 비교
+4. 로그인 버튼을 클릭시 user.pw의 값과 input의 값을 비교
 
-  if (isValid) {
-    // 통신
-    try {
-      if (submitData({ id: inputId.value, pw: inputPw.value, state: loginState.checked, ip: loginIp.checked })) {
-        // 로그인 성공
-        window.location.href = "welcome.html";
-      } else {
-        alert("로그인 실패...");
-      }
-    } catch (err) {
-      alert("통신 에러 발생...");
-    }
-  }
 
-  return false;
-}
+5. 두 값이 일치 한다면 다음 페이지(welcome.html)로 이동
+*/
 
-// 로그인 버튼에 이벤트 적용
-const loginBtn = getNode(".login-form .btn-login");
-loginBtn.onclick = handlLogin;
+const emailInput = document.querySelector("#userEmail");
+const pwInput = document.querySelector("#userPassword");
+const loginButton = document.querySelector(".btn-login");
 
-// 서버 통신
-function submitData(data) {
-  console.log(data);
-  if (data.id != user.id) return false;
-  if (data.pw != user.pw) return false;
+let emailPass = false;
+let pwPass = false;
 
-  return true;
-}
-
-// validation -------------------------------------
-// email 정규표현식을 사용한 validation
+// @ 기호 포함, .포함 이후 2글자 이상
 function emailReg(text) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return re.test(String(text).toLowerCase());
 }
 
-// pw 정규표현식을 사용한 validation
+// 최소 6글자 이상, 0~9숫자 1개 이상 포함, 특수기호 1개 이상 포함
 function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
+
+function handleCheckEmail() {
+  let value = this.value;
+
+  if (emailReg(value)) {
+    this.classList.remove("is--invalid");
+    emailPass = true;
+  } else {
+    this.classList.add("is--invalid");
+    emailPass = false;
+  }
+}
+
+function handleCheckPw() {
+  let value = this.value;
+
+  if (pwReg(value)) {
+    this.classList.remove("is--invalid");
+    pwPass = true;
+  } else {
+    this.classList.add("is--invalid");
+    pwPass = false;
+  }
+}
+
+function handleLogin(e) {
+  e.preventDefault();
+
+  if (emailPass && pwPass) {
+    try {
+      const id = emailInput.value;
+      const pw = pwInput.value;
+      const getUserId = user.id;
+      const getUserPw = user.pw;
+
+      if (id === getUserId && pw === getUserPw) {
+        console.log("로그인 성공");
+        location.href = "welcome.html";
+      }
+    } catch (e) {}
+  }
+}
+
+emailInput.addEventListener("input", handleCheckEmail);
+pwInput.addEventListener("input", handleCheckPw);
+loginButton.addEventListener("click", handleLogin);
